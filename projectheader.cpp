473a260bfc5856135804d9468cfb322ba687698d
IRTAZA MANZOOR <irtaza.manzoor1203@gmail.com>
2022-12-15 19:01:14 +0500

    Submitted

---
+++ projectheader.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"*************************************************"<<endl;
+cout<<"*                  Welcome to                   *"<<endl;
+cout<<"*                  UET Lahore                   *"<<endl;
+cout<<"*                                               *"<<endl;
+cout<<"*************************************************"<<endl;
+}
